613f9db35aac20821d81a39f783aa6933955d64d
Michael Allan <michaelallan@optushome.com.au>
2012-08-26 15:55:04 +1000

    Add full show/hide behaviour for description toggle links

--- spec/javascripts/jasmine_examples/togglerSpec.js
+++ spec/javascripts/jasmine_examples/togglerSpec.js
@@ -1,11 +1,32 @@
 describe('Trip detail toggler', function () {
-  describe('clicking a show link', function () {
-    it('should show the trip description', function () {
-      loadFixtures('one_index_trip.html');
-      init();
 
-      $('.detail_toggle').click();
-      expect($('.detail')).not.toHaveClass('hidden');   // NO, this is WRONG WRONG WRONG!!!!
+    beforeEach(function () {
+        loadFixtures('one_index_trip.html');
+        init();
+        $('.detail_toggle').click();
     });
-  });
+
+    describe('Clicking the show link', function () {
+        it('shows the trip description', function () {
+            expect($('.detail')).not.toHaveClass('hidden');   // NO, this is WRONG WRONG WRONG!!!!
+        });
+
+        it('changes the link action to "Hide"', function () {
+            expect($('.detail_toggle')).toHaveText('Hide Details');
+        });
+    });
+
+    describe('Clicking the show link again', function () {
+        beforeEach(function () {
+            $('.detail_toggle').click();
+        });
+
+        it('hides the description', function () {
+            expect($('.detail')).toBeHidden();
+        });
+
+        it('changes the link action back to "Show"', function () {
+            expect($('.detail_toggle')).toHaveText('Show Details');
+        });
+    });
 });
--- spec/javascripts/toggler.js
+++ spec/javascripts/toggler.js
@@ -3,6 +3,13 @@ function init() {
 
     toggle_links.click(function (ev) {
         var detail = $(this).siblings('.detail');
+
+        if (detail.hasClass('hidden')) {
+            $(this).text('Hide Details');
+        } else {
+            $(this).text('Show Details');
+        }
+
         detail.toggleClass('hidden');
 
         ev.preventDefault();
